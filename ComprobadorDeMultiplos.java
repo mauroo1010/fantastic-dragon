317b7053d70615890813f95ec86895c29ed1dd03
mauroo1010 <maurogutierreztormo@gmail.com>
2024-01-15 09:25:25 +0100

    02

--- ComprobadorDeMultiplos.java
+++ ComprobadorDeMultiplos.java
@@ -8,13 +8,51 @@
  */
 public class ComprobadorDeMultiplos
 {
-    public boolean esMultiploDe2 (int numero) {
-        return true;
+    public boolean esMultiploDe2 (int a) {
+        boolean multiploDe2;
+        int resultado = a % 2;
+        if (a >= 2) {
+            if (resultado == 0) {
+                multiploDe2 = true;
+            }
+            else{
+                multiploDe2 = false;
+            }
+        }
+        else{
+            multiploDe2 = false;
+        }
+        return multiploDe2;
     }
-    public boolean esMultiploDe5 (int numero) {
-        return true;
+    public boolean esMultiploDe5 (int a) {
+         boolean multiploDe5;
+        int resultado = a % 5;
+        if (a >= 5) {
+            if (resultado == 0) {
+                multiploDe5 = true;
+            }
+            else{
+                multiploDe5 = false;
+            }
+        }
+        else{
+            multiploDe5 = false;
+        }
+        return multiploDe5;
     }
     public boolean esMultiploDe (int a, int b) {
-        return true;
-    }
+         boolean esMultiploDe;
+        if (a < b || b == 0 || a < 0 || b < 0) {
+            esMultiploDe = false;
+        }
+        else{
+            if (a % b == 0) {
+                esMultiploDe = true;
+            }
+            else {
+                esMultiploDe = false;
+            }
+        }
+        return esMultiploDe;
+    }    
 }
